2dc7b158a2e86e8398b49288070d11a48c011124
tammynanda <tammynanda.com>
2022-12-07 20:17:00 +0900

    함수 만들었어요!

---
+++ utils.py
@@ -0,0 +1,19 @@
+"""
+코드 작성 일자 : 2022년 9월 8일
+코드 작성자 : 정다미
+코드 이른 : utils.py
+코드 목적 : 유용한 함수를 따로 저장하여 두고 나중에 불러와 사용하기 위함
+"""
+
+
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x -1)
+
+
+def gugudan(x):
+    for i in range(9):
+        print((i + 1) * x)
+
